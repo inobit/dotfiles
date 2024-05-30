ca9ed1e268e48b6f26c2d9812c469e5b10d2c5e7
wgx <489303870@qq.com>
2024-05-30 17:02:21 +0800

    nvim sql formatter config

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -39,6 +39,8 @@ return { -- Autoformat
           language = "mysql",
           tabWidth = 2,
           keywordCase = "upper",
+          dataTypeCase = "upper",
+          functionCase = "upper",
           linesBetweenQueries = 2,
           paramTypes = { named = { ":" } },
         },
