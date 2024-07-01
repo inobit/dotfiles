dbcbad0394c788edd65d4f4b855049e08b45c393
anyw <any.wgx@qq.com>
2024-07-01 08:54:51 +0800

    update newos/install.sh

--- newos/install.sh
+++ newos/install.sh
@@ -128,7 +128,7 @@ if [ "$docker" = "y" ]; then
 
 	# Add Docker's official GPG key:
 	sudo apt-get update
-	sudo apt-get install ca-certificates curl
+	sudo apt-get install ca-certificates curl -y
 	sudo install -m 0755 -d /etc/apt/keyrings
 	sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 	sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -141,7 +141,7 @@ if [ "$docker" = "y" ]; then
 		sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 	sudo apt-get update
 	# install the latest version
-	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 fi
 
 echo "install zsh"
