cfa30cb66ae72d092f19db639a48f6818a4c1223
anyw <any.wgx@qq.com>
2024-11-19 13:49:29 +0800

    markdown render config update

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -5,6 +5,10 @@ return {
     "nvim-tree/nvim-web-devicons",
   }, -- if you prefer nvim-web-devicons
   opts = {
+    -- Vim modes that will show a rendered view of the markdown file
+    -- All other modes will be unaffected by this plugin
+    -- render_modes = { "n", "c" },
+    render_modes = true,
     code = {
       sign = false,
       width = "full",
