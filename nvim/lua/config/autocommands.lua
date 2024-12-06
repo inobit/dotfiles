02fcbc893f3fd0dbcab1ea9eb5158338cf890dd5
anyw <any.wgx@qq.com>
2024-12-06 15:05:23 +0800

    nvim bug fix

--- nvim/lua/config/autocommands.lua
+++ nvim/lua/config/autocommands.lua
@@ -2,27 +2,34 @@
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
   callback = function()
     vim.highlight.on_yank()
   end,
 })
 -- 修改时mark，达到lastchange的效果
-vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedT", "TextChangedP", "TextChangedI" }, {
-  desc = "last change",
-  group = vim.api.nvim_create_augroup("textChange", { clear = true }),
-  callback = function(event)
-    -- 需要排除浮动窗口
-    local relative = vim.api.nvim_win_get_config(vim.api.nvim_get_current_win()).relative
-    -- 排除NvimTree(nofile) terminal prompt
-    local buftype = vim.bo[event.buf].buftype
-    if relative == "" and not vim.tbl_contains({ "nofile", "terminal", "prompt" }, buftype) then
-      local x, y = unpack(vim.api.nvim_win_get_cursor(0))
-      -- 跨buffer需要使用大写name
-      vim.api.nvim_buf_set_mark(0, "Z", x, y, {})
-    end
-  end,
-})
+vim.api.nvim_create_autocmd(
+  { "TextChanged", "TextChangedT", "TextChangedP", "TextChangedI" },
+  {
+    desc = "last change",
+    group = vim.api.nvim_create_augroup("textChange", { clear = true }),
+    callback = function(event)
+      -- 需要排除浮动窗口
+      local relative =
+        vim.api.nvim_win_get_config(vim.api.nvim_get_current_win()).relative
+      -- 排除NvimTree(nofile) terminal prompt
+      local buftype = vim.bo[event.buf].buftype
+      if
+        relative == ""
+        and not vim.tbl_contains({ "nofile", "terminal", "prompt" }, buftype)
+      then
+        local x, y = unpack(vim.api.nvim_win_get_cursor(0))
+        -- 跨buffer需要使用大写name
+        vim.api.nvim_buf_set_mark(0, "Z", x, y, {})
+      end
+    end,
+  }
+)
 -- 如果最后一个buffer是NvimTree则直接退出
 vim.api.nvim_create_autocmd("BufEnter", {
   group = vim.api.nvim_create_augroup("NvimTreeClose", { clear = true }),
@@ -31,8 +38,12 @@ vim.api.nvim_create_autocmd("BufEnter", {
     local layout = vim.api.nvim_call_function("winlayout", {})
     if
       layout[1] == "leaf"
-      and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree"
       and layout[3] == nil
+      and vim.api.nvim_get_option_value(
+          "filetype",
+          { buf = vim.api.nvim_win_get_buf(layout[2]) }
+        )
+        == "NvimTree"
     then
       -- 提示保存
       vim.cmd "confirm quit"
@@ -44,7 +55,17 @@ vim.api.nvim_create_autocmd("BufEnter", {
   group = vim.api.nvim_create_augroup("Colorizer", {
     clear = true,
   }),
-  pattern = { "*.html", "*.css", "*.scss", "*.less", "*.sass", "*.ts", "*.js", "*.tsx", "*.jsx" },
+  pattern = {
+    "*.html",
+    "*.css",
+    "*.scss",
+    "*.less",
+    "*.sass",
+    "*.ts",
+    "*.js",
+    "*.tsx",
+    "*.jsx",
+  },
   callback = function()
     local status, colorizer = pcall(require, "colorizer")
     if status then
