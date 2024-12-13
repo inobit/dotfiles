4730f9a6bc8f1911c98b0a4c4e227e1a903f1210
wgx <489303870@qq.com>
2024-12-13 09:40:05 +0800

    increase conform timeout_ms

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -4,7 +4,7 @@ return { -- Autoformat
   opts = {
     notify_on_error = false,
     format_on_save = {
-      timeout_ms = 500,
+      timeout_ms = 2500,
       lsp_fallback = true,
     },
     formatters_by_ft = {
