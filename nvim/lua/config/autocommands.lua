d7f338698fb86d3d62ed298bcdd08348cfb9f883
anyw <any.wgx@qq.com>
2025-01-03 11:21:18 +0800

    chore

--- nvim/lua/config/autocommands.lua
+++ nvim/lua/config/autocommands.lua
@@ -2,7 +2,7 @@
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+  group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
   callback = function()
     vim.highlight.on_yank()
   end,
