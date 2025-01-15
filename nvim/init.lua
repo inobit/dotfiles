13a6d2ab88314e190c8e323a31557dfa922080bf
anyw <any.wgx@qq.com>
2025-01-15 17:12:46 +0800

    refactor: move filetype globals neovide  to plugin dir

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,11 +2,8 @@ if not vim.g.vscode then
   require "config.options"
   pcall(require, "config.local-options")
   require "config.keymaps"
-  require "config.filetype"
   require "config.plugin-manager"
   require "config.autocommands"
-  require "config.globals"
-  require "config.neovide"
 else
   require "config.options"
   require "config.vscode"
