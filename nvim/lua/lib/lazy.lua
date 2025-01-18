5a6ffbb7bd3a1c277522bff7aaa48def293c88c7
anyw <any.wgx@qq.com>
2025-01-18 22:40:01 +0800

    chore: how plugin work

--- nvim/lua/lib/lazy.lua
+++ nvim/lua/lib/lazy.lua
@@ -17,10 +17,10 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 --[[
-     :help runtimepath 中对于搜索路径和文件有相关说明(neovim使用XDG规范 ，默认使用~/.config,~/.local/share之类的，本质上install plugin就是加入到runtimepath中
-     plugin的使用一般分为2部分，load，可以理解为启用了，和模块化引用
-     load实际上只是执行了plugin_name.nvim/plugin/plugin_name.lua文件，这个执行是自动的，打开nvim时会立即或者滞后（懒加载）执行插件的这个文件
-     require plugin，则是引入这个模块，也就是执行了plugin_name.nvim/lua/plugin_name.lua (或plugin_name/init.lua)文件，因为lua目录在package.path中，所以可以require，没有什么魔法
-     这2个部分是独立的，并没有相互依赖，只要lua加入了runtimepath，就可以require，并不需要先执行load,只不过load是自动的，所以一般都是先执行了
-     分为2个部分的目的也很简单，按需提供，而不是都放在plugin/plugin_name.lua下，startup时全部执行
+关于插件加载逻辑：
+1. nvim启动时会去默认的runtimepath(:h runtimepath)下的相关目录执行脚本（不同目录有不同的作用,比如plugin,ftplugin,color,syntax等等，有些会自动执行有些按需执行）
+2. 相关after/目录也在runtimepath中(after中也有系列目录比如plugin,ftplugi,color等等),但是在最后面,runtimepath按顺序执行
+3. 相关脚本触发Lazy管理插件,将相关插件目录以及插件目录/after/加入到runtimepath中,同时执行这些runtimepath下的相关目录下的脚本(类似1)
+4. after目录(默认和插件的)排在runtimepath后面,最后执行,一般用于后置规则,覆盖一些默认配置
+5. require引用模块,调用API,是按照runtimepath中的顺序去对应的lua文件夹下找module.lua或module/init.lua
 --]]
