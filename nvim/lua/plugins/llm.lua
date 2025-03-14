f000451cbd802d6e30ca3cb514ee60d1db8f8fab
anyw <any.wgx@qq.com>
2025-03-14 13:21:05 +0800

    feat: update llm

--- nvim/lua/plugins/llm.lua
+++ nvim/lua/plugins/llm.lua
@@ -8,6 +8,7 @@ return {
     keys = {
       -- stylua: ignore start
       { "<leader>mm", "<Cmd>LLM Chat<CR>", desc = "LLM: chat start" },
+      { "<leader>mt", "<Cmd>LLM ShutDown<CR>", desc = "LLM: chat shutdown" },
       { "<leader>ma", "<Cmd>LLM Auth<CR>", desc = "LLM: chat auth" },
       { "<leader>mn", "<Cmd>LLM New<CR>", desc = "LLM: chat new" },
       { "<leader>mx", "<Cmd>LLM Clear<CR>", desc = "LLM: chat clear(unsaved)" },
@@ -16,8 +17,20 @@ return {
       { "<leader>md", "<Cmd>LLM Delete<CR>", desc = "LLM: delete session" },
       { "<leader>mr", "<Cmd>LLM Rename<CR>", desc = "LLM: rename session" },
       { "<leader>mv", "<Cmd>LLM Servers<CR>", desc = "LLM: select server" },
+      {
+        "<leader>tsz", function() require("inobit.llm.translate").translate_and_repalce "E2Z" end, mode = { "n", "v" }, desc = "LLM: translate to ZH",
+      },
+      {
+        "<leader>tse", function() require("inobit.llm.translate").translate_and_repalce "Z2E" end, mode = { "n", "v" }, desc = "LLM: translate to EN",
+      },
+      {
+        "<leader>tsc", function() require("inobit.llm.translate").translate_and_repalce "Z2E_CAMEL" end, mode = { "n", "v" }, desc = "LLM: translate to VAR_CAMEL",
+      },
+      {
+        "<leader>tsu", function() require("inobit.llm.translate").translate_and_repalce "Z2E_UNDERLINE" end, mode = { "n", "v" }, desc = "LLM: translate to VAR_UNDERLINE", },
       -- stylua: ignore end
     },
+    cmd = { "LLM", "TS" },
     name = "inobit-llm.nvim",
     main = "inobit/llm",
     opts = {},
