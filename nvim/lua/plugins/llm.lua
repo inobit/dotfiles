2e496d2913d6de1f3910f80722936724843a2556
anyw <any.wgx@qq.com>
2025-03-19 20:52:18 +0800

    feat: llm config

--- nvim/lua/plugins/llm.lua
+++ nvim/lua/plugins/llm.lua
@@ -7,7 +7,7 @@ return {
     },
     keys = {
       -- stylua: ignore start
-      { "<leader>mm", "<Cmd>LLM Chat<CR>", desc = "LLM: chat start" },
+      { "<leader>mc", "<Cmd>LLM Chat<CR>", desc = "LLM: chat start" },
       { "<leader>mt", "<Cmd>LLM ShutDown<CR>", desc = "LLM: chat shutdown" },
       { "<leader>ma", "<Cmd>LLM Auth<CR>", desc = "LLM: chat auth" },
       { "<leader>mn", "<Cmd>LLM New<CR>", desc = "LLM: chat new" },
@@ -18,16 +18,23 @@ return {
       { "<leader>mr", "<Cmd>LLM Rename<CR>", desc = "LLM: rename session" },
       { "<leader>mv", "<Cmd>LLM Servers<CR>", desc = "LLM: select server" },
       {
-        "<leader>tsz", function() require("inobit.llm.translate").translate_and_repalce "E2Z" end, mode = { "n", "v" }, desc = "LLM: translate to ZH",
+        "<leader>tsz", function() require("inobit.llm.translate").translate_and_repalce "E2Z" end, mode = { "n", "v" }, desc = "LLM: translate and replace to ZH",
       },
       {
-        "<leader>tse", function() require("inobit.llm.translate").translate_and_repalce "Z2E" end, mode = { "n", "v" }, desc = "LLM: translate to EN",
+        "<leader>tse", function() require("inobit.llm.translate").translate_and_repalce "Z2E" end, mode = { "n", "v" }, desc = "LLM: translate and replace to EN",
       },
       {
         "<leader>tsc", function() require("inobit.llm.translate").translate_and_repalce "Z2E_CAMEL" end, mode = { "n", "v" }, desc = "LLM: translate to VAR_CAMEL",
       },
       {
-        "<leader>tsu", function() require("inobit.llm.translate").translate_and_repalce "Z2E_UNDERLINE" end, mode = { "n", "v" }, desc = "LLM: translate to VAR_UNDERLINE", },
+        "<leader>tsu", function() require("inobit.llm.translate").translate_and_repalce "Z2E_UNDERLINE" end, mode = { "n", "v" }, desc = "LLM: translate to VAR_UNDERLINE",
+      },
+      {
+        "<leader>tpz", function() require("inobit.llm.translate").translate_and_print "E2Z" end, mode = { "n", "v" }, desc = "LLM: translate and print to ZH",
+      },
+      {
+        "<leader>tpe", function() require("inobit.llm.translate").translate_and_print "Z2E" end, mode = { "n", "v" }, desc = "LLM: translate and print to EN",
+      },
       -- stylua: ignore end
     },
     cmd = { "LLM", "TS" },
@@ -36,7 +43,7 @@ return {
     opts = {
       servers = {
         {
-          server = "Qwen",
+          server = "Qwen-SiliconFlow",
           base_url = "https://api.siliconflow.cn/v1",
           model = "Qwen/Qwen2.5-Coder-7B-Instruct",
           stream = true,
