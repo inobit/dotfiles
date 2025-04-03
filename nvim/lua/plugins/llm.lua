aa20ac7a3ea0eb40165a46850828f94115e366a7
anyw <any.wgx@qq.com>
2025-04-03 20:10:49 +0800

    update

--- nvim/lua/plugins/llm.lua
+++ nvim/lua/plugins/llm.lua
@@ -8,26 +8,20 @@ return {
     keys = {
       -- stylua: ignore start
       { "<leader>mc", "<Cmd>LLM Chat<CR>", desc = "LLM: chat start" },
-      { "<leader>mt", "<Cmd>LLM ShutDown<CR>", desc = "LLM: chat shutdown" },
-      { "<leader>ma", "<Cmd>LLM Auth<CR>", desc = "LLM: chat auth" },
-      { "<leader>mn", "<Cmd>LLM New<CR>", desc = "LLM: chat new" },
-      { "<leader>mx", "<Cmd>LLM Clear<CR>", desc = "LLM: chat clear(unsaved)" },
-      { "<leader>mS", "<Cmd>LLM Save<CR>", desc = "LLM: chat  save" },
       { "<leader>ms", "<Cmd>LLM Sessions<CR>", desc = "LLM: select session" },
-      { "<leader>md", "<Cmd>LLM Delete<CR>", desc = "LLM: delete session" },
-      { "<leader>mr", "<Cmd>LLM Rename<CR>", desc = "LLM: rename session" },
-      { "<leader>mv", "<Cmd>LLM Servers<CR>", desc = "LLM: select server" },
+      { "<leader>mv", "<Cmd>LLM ChatServers<CR>", desc = "LLM: select chat server" },
+      { "<leader>mt", "<Cmd>LLM TSServers<CR>", desc = "LLM: select translate server" },
       {
-        "<leader>ts", function() require("inobit.llm.translate").translate_in_buffer(true)  end, mode = { "n", "v" }, desc = "LLM: translate and replace",
+        "<leader>ts", function() require("inobit.llm.api").translate_in_buffer(true)  end, mode = { "n", "v" }, desc = "LLM: translate and replace",
       },
       {
-        "<leader>tc", function() require("inobit.llm.translate").translate_in_buffer(true, "Z2E_CAMEL") end, mode = { "n", "v" }, desc = "LLM: translate to VAR_CAMEL",
+        "<leader>tc", function() require("inobit.llm.api").translate_in_buffer(true, "Z2E_CAMEL") end, mode = { "n", "v" }, desc = "LLM: translate to VAR_CAMEL",
       },
       {
-        "<leader>tu", function() require("inobit.llm.translate").translate_in_buffer(true, "Z2E_UNDERLINE") end, mode = { "n", "v" }, desc = "LLM: translate to VAR_UNDERLINE",
+        "<leader>tu", function() require("inobit.llm.api").translate_in_buffer(true, "Z2E_UNDERLINE") end, mode = { "n", "v" }, desc = "LLM: translate to VAR_UNDERLINE",
       },
       {
-        "<leader>tp", function() require("inobit.llm.translate").translate_in_buffer(false)  end, mode = { "n", "v" }, desc = "LLM: translate and print",
+        "<leader>tp", function() require("inobit.llm.api").translate_in_buffer(false)  end, mode = { "n", "v" }, desc = "LLM: translate and print",
       },
       -- stylua: ignore end
     },
@@ -37,14 +31,25 @@ return {
     opts = {
       servers = {
         {
-          server = "Qwen-SiliconFlow",
+          server = "SiliconFlow",
           base_url = "https://api.siliconflow.cn/v1/chat/completions",
-          model = "Qwen/Qwen2.5-Coder-7B-Instruct",
+          api_key_name = "SILICONFLOW_API_KEY",
+          models = { "Qwen/Qwen2.5-Coder-7B-Instruct" },
           stream = true,
           multi_round = true,
           user_role = "user",
         },
+        {
+          server = "test-server",
+          base_url = "http://localhost:8000/ai_stream",
+          api_key_name = "TEST_API_KEY",
+          models = { "test-model" },
+          stream = true,
+          multi_round = true,
+        },
       },
+      default_server = "test-server@test-model",
+      default_translate_server = "SiliconFlow@deepseek-ai/DeepSeek-V3",
     },
   },
 }
