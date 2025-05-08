1ef723c70ebb705ea5740790f6d87558e214373f
anyw <any.wgx@qq.com>
2025-05-08 15:54:31 +0800

    feat: auto set python3.host.prog

---
+++ nvim/plugin/env.lua
@@ -0,0 +1,61 @@
+local function setup_nvim_venv()
+  local uv_exists = vim.fn.executable "uv" == 1
+
+  if not uv_exists then
+    vim.notify("the uv command does not exist. please install uv first.", vim.log.levels.ERROR)
+    return false
+  end
+
+  local data_path = vim.fn.stdpath "data"
+
+  -- determine the path separator based on the operating system
+  local is_windows = vim.fn.has "win32" == 1 or vim.fn.has "win64" == 1
+  local path_sep = is_windows and "\\" or "/"
+  local venv_path = data_path .. path_sep .. "venv"
+
+  -- check if the virtual environment directory exists
+  local venv_exists = vim.fn.isdirectory(venv_path) == 1
+
+  if not venv_exists then
+    -- create a virtual environment
+    vim.notify("creating a virtual environment...", vim.log.levels.INFO)
+    local create_cmd = "uv venv --python 3.12 " .. vim.fn.shellescape(venv_path .. path_sep .. ".venv")
+    local create_result = vim.fn.system(create_cmd)
+
+    if vim.v.shell_error ~= 0 then
+      vim.notify("virtual environment creation failed: " .. create_result, vim.log.levels.ERROR)
+      return false
+    end
+
+    vim.notify("virtual environment created successfully" .. venv_path, vim.log.levels.INFO)
+
+    -- install pynvim
+    vim.notify("installing pynvim...", vim.log.levels.INFO)
+
+    local current_dir = vim.fn.getcwd()
+
+    -- switch to the virtual environment directory
+    vim.fn.chdir(venv_path)
+
+    -- execute the install command
+    local install_cmd = "uv pip install pynvim"
+    local install_result = vim.fn.system(install_cmd)
+
+    -- return to the previous directory
+    vim.fn.chdir(current_dir)
+
+    if vim.v.shell_error ~= 0 then
+      vim.notify("installing pynvim failed: " .. install_result, vim.log.levels.ERROR)
+      return false
+    end
+
+    vim.notify("pynvim installation successful", vim.log.levels.INFO)
+
+    return true, venv_path .. path_sep .. ".venv" .. path_sep .. "bin" .. path_sep .. "python3"
+  else
+    -- is exists
+    return true, venv_path .. path_sep .. ".venv" .. path_sep .. "bin" .. path_sep .. "python3"
+  end
+end
+
+_, vim.g.python3_host_prog = setup_nvim_venv()
