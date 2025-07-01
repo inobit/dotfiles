9d24f17da66ff843c17785db71b9048426c01e79
anyw <any.wgx@qq.com>
2025-07-01 16:03:55 +0800

    chore: delete sudo plugin keymap

--- nvim/lua/plugins/sudo.lua
+++ nvim/lua/plugins/sudo.lua
@@ -1,9 +1,9 @@
 return {
   "lambdalisue/suda.vim",
   event = "VeryLazy",
-  init = function()
-    if vim.fn.has "win32" == 0 then
-      vim.keymap.set("c", "W", "<esc>:SudaWrite<CR>", { silent = true })
-    end
-  end,
+  -- init = function()
+  --   if vim.fn.has "win32" == 0 then
+  --     vim.keymap.set("c", "W", "<esc>:SudaWrite<CR>", { silent = true })
+  --   end
+  -- end,
 }
