1377ec8d868106b61cb2790fbfaa7de78397cec8
anyw <any.wgx@qq.com>
2025-09-16 15:00:26 +0800

    feat: add bqf plugin

---
+++ nvim/lua/plugins/quickfix.lua
@@ -0,0 +1,11 @@
+return {
+  "kevinhwang91/nvim-bqf",
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  ft = "qf",
+  opts = {
+    func_map = {
+      split = "<M-h>",
+      vsplit = "<M-v>",
+    },
+  },
+}
