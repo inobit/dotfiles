a85d54ab7609217482607da509972bd2dcff7c5d
anyw <any.wgx@qq.com>
2025-10-13 17:19:12 +0800

    feat: add pre-commit hooks

---
+++ scripts/cleanup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+for file in "$@"; do
+	if [[ "$file" == "mihomo/config.yaml" ]]; then
+		sed -i -E -e '/^proxy-providers/{n;s/^(\s*)\S+.*/\1<subscription name>:/;n;n;s/^(\s*url:\s*).*/\1<subscription url>/;n;s/^(\s*path:\s*.*\/).*/\1<subscription name>.yaml/}' "$file"
+		git add "$file"
+		echo "$file has been cleaned"
+	fi
+done
