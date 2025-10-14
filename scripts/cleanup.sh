03222c294e0ae728194099e325c25aefa1d7f655
anyw <any.wgx@qq.com>
2025-10-14 11:11:04 +0800

    fix: remove secret

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -4,7 +4,8 @@ set -e
 
 for file in "$@"; do
 	if [[ "$file" == "mihomo/config.yaml" ]]; then
-		sed -i -E -e '/^proxy-providers/{n;s/^(\s*)\S+.*/\1<subscription name>:/;n;n;s/^(\s*url:\s*).*/\1<subscription url>/;n;s/^(\s*path:\s*.*\/).*/\1<subscription name>.yaml/}' "$file"
+
+		sed -i -E -e 's/^(secret:\s*).*/\1<secret>/' -e '/^proxy-providers/{n;s/^(\s*)\S+.*/\1<subscription name>:/;n;n;s/^(\s*url:\s*).*/\1<subscription url>/;n;s/^(\s*path:\s*.*\/).*/\1<subscription name>.yaml/}' "$file"
 		git add "$file"
 		echo "$file has been cleaned"
 	fi
