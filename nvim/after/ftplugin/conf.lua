4eca6fe45180c3f341118bd0dc8775db546f6b67
anyw <any.wgx@qq.com>
2025-10-17 15:52:18 +0800

    fix: ignore = in url

--- nvim/after/ftplugin/conf.lua
+++ nvim/after/ftplugin/conf.lua
@@ -3,9 +3,12 @@ function _G.conf_formatter()
   local count = vim.v.count
   local end_line = start_line + count - 1
   -- delete leading whitespace
-  vim.cmd(string.format("%d,%ds/^\\s*//", start_line, end_line))
+  vim.cmd(string.format("%d,%d%s", start_line, end_line, "s/^\\s*//"))
   -- add spaces around '='
-  vim.cmd(string.format("%d,%ds/\\v(\\S)(\\s*)\\=(\\s*)(\\S)/\\1 = \\4/g", start_line, end_line))
+  local left_equals = "s/\\v(\\S)(\\s*)"
+  local equals_sign = "((\\w+:\\/\\/[^ ,]+)@<!\\=)" -- ignore '=' in url
+  local right_equals = "(\\s*)(\\S)/\\1 = \\6/g"
+  vim.cmd(string.format("%d,%d%s%s%s", start_line, end_line, left_equals, equals_sign, right_equals))
   -- add spaces on the right of ','
   vim.cmd(string.format("%d,%ds/\\v(\\S)(\\s*)\\,(\\s*)(\\S)/\\1, \\4/g", start_line, end_line))
   vim.cmd "nohlsearch"
