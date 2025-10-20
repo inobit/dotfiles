582d3ff03999a1ca6375391600ded4c6ca6e73c1
anyw <any.wgx@qq.com>
2025-10-20 10:26:50 +0800

    optimize: cd project root dir before run/check

--- nvim/after/ftplugin/rust.lua
+++ nvim/after/ftplugin/rust.lua
@@ -1,3 +1,16 @@
-require("lib.run").register_run_keymap("cargo run", "<leader>rr")
+local function get_root_dir()
+  local cargo_toml = vim.fn.findfile("Cargo.toml", ".;")
+  if cargo_toml == "" then
+    return vim.fn.getcwd()
+  end
+  return vim.fn.fnamemodify(cargo_toml, ":p:h")
+end
+
+-- cargo run
+require("lib.run").register_run_keymap(function()
+  return string.format("cd %s && %s", get_root_dir(), "cargo run")
+end, "<leader>rr")
 -- cargo check
-require("lib.run").register_run_keymap("cargo check", "<leader>rc")
+require("lib.run").register_run_keymap(function()
+  return string.format("cd %s && %s", get_root_dir(), "cargo check")
+end, "<leader>rc")
