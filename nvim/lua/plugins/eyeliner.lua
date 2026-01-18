21061ce83f2a9d6b7d9a323ece293125dc2f3db6
inobit <inoechoc@gmail.com>
2026-01-18 11:56:19 +0800

    refactor: use folk repo and update config

--- nvim/lua/plugins/eyeliner.lua
+++ nvim/lua/plugins/eyeliner.lua
@@ -1,10 +1,18 @@
 return {
-  "jinh0/eyeliner.nvim",
-  keys = { "f", "F", "t", "T" },
+  "cosmicbuffalo/eyeliner.nvim",
+  -- keys = { "f", "F", "t", "T" },
   config = function()
     require("eyeliner").setup {
-      highlight_on_key = true, -- show highlights only after keypress
       dim = false, -- dim all other characters if set to true (recommended!)
+      disabled_filetypes = {
+        "dashboard",
+        "snacks_dashboard",
+        "dapui_breakpoints",
+        "dapui_stacks",
+        "dapui_scopes",
+        "dapui_watches",
+        "dap-repl",
+      },
     }
   end,
 }
