e08f343c62ae9e784bac2da120ba8536403eb973
inobit <inoechoc@gmail.com>
2026-03-27 13:58:50 +0800

    feat: remove codeium

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -1,44 +1,5 @@
 return {
   {
-    "Exafunction/windsurf.nvim",
-    event = { "InsertEnter" },
-    cond = not vim.g.disable_ai_completion,
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "hrsh7th/nvim-cmp",
-    },
-    opts = {
-      enable_cmp_source = "codeium" ~= vim.g.ai_inline_completion_engine,
-      virtual_text = {
-        enabled = "codeium" == vim.g.ai_inline_completion_engine,
-        filetypes = { bigfile = false, dap_repl = false, dotenv = false },
-        key_bindings = {
-          accept = "<M-y>",
-          accept_line = "<M-l>",
-          accept_word = "<M-j>",
-          clear = "<M-e>",
-          next = "<M-]>",
-          prev = "<M-[>",
-        },
-      },
-      workspace_root = {
-        use_lsp = true,
-        find_root = nil,
-        paths = {
-          ".bzr",
-          ".git",
-          ".hg",
-          ".svn",
-          "_FOSSIL_",
-          "package.json",
-        },
-      },
-    },
-    config = function(_, opts)
-      require("codeium").setup(opts)
-    end,
-  },
-  {
     "supermaven-inc/supermaven-nvim",
     event = { "InsertEnter" },
     cond = not vim.g.disable_ai_completion,
