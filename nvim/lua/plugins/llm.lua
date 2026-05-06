4c3fd5ab2d2c9ad7428ec4fa67c6c56eef9341a8
inobit <inoechoc@gmail.com>
2026-05-06 09:44:47 +0800

    refactor(nvim): new llm api

--- nvim/lua/plugins/llm.lua
+++ nvim/lua/plugins/llm.lua
@@ -7,10 +7,9 @@ return {
     },
     keys = {
       -- stylua: ignore start
-      { "<leader>mc", "<Cmd>LLM Chat<CR>", desc = "LLM: chat start" },
-      { "<leader>ms", "<Cmd>LLM Sessions<CR>", desc = "LLM: select session" },
-      { "<leader>ma", "<Cmd>LLM ChatServers<CR>", desc = "LLM: select chat server" },
-      { "<leader>mt", "<Cmd>LLM TSServers<CR>", desc = "LLM: select translate server" },
+      { "<leader>at", "<Cmd>LLM Toggle<CR>", desc = "LLM: chat toggle" },
+      { "<leader>as", "<Cmd>LLM Sessions<CR>", desc = "LLM: select session" },
+      { "<leader>ap", "<Cmd>LLM Providers<CR>", desc = "LLM: select provider" },
       {
         "<leader>ts", function() require("inobit.llm.api").translate_in_buffer(true)  end, mode = { "n", "v" }, desc = "LLM: translate and replace",
       },
@@ -30,59 +29,29 @@ return {
     main = "inobit.llm",
     opts = function()
       local opts = {
-        servers = {
-          {
-            server = "OpenRouter",
-            base_url = "https://openrouter.ai/api/v1/chat/completions",
-            api_key_name = "OPENROUTER_API_KEY",
-            models = {
-              { model = "anthropic/claude-opus-4.5", temperature = 0.4 },
-              { model = "openai/gpt-5.2", temperature = 0.4 },
-              { model = "google/gemini-3-pro-preview", temperature = 0.4 },
-              { model = "google/gemini-2.5-flash", temperature = 0.4 },
-              { model = "google/gemini-2.0-flash-001", max_tokens = 8192, temperature = 0.6 },
-              { model = "x-ai/grok-code-fast-1", max_tokens = 8192, temperature = 0.6 },
-            },
-            max_tokens = 4096,
-            stream = true,
-            multi_round = true,
-            user_role = "user",
-          },
-          {
-            server = "nvidia",
-            base_url = "https://integrate.api.nvidia.com/v1/chat/completions",
-            api_key_name = "NVIDIA_API_KEY",
-            models = {
-              { model = "minimaxai/minimax-m2.7", max_tokens = 8192, temperature = 0.6 },
-              { model = "z-ai/glm5", max_tokens = 8192, temperature = 0.6 },
-            },
-            temperature = 1,
-            stream = true,
-            multi_round = true,
-            user_role = "user",
+        -- 场景默认 Provider
+        scenario_defaults = {
+          chat = "OpenRouter",
+          translate = vim.g.my_deeplx and "DeepL" or "OpenRouter",
+        },
+        providers = {
+          --DeepL
+          DeepL = {
+            base_url = vim.g.my_deeplx and vim.g.my_deeplx_base_url or "https://api.deepl.com/v2/translate",
+            api_key_name = false,
+            default_model = "deepxl",
+            fetch_models = false,
           },
         },
-        default_server = "nvidia@z-ai/glm5",
-        default_translate_server = vim.g.my_deeplx and "DeepL@DeepLX" or "OpenRouter@google/gemini-2.0-flash-001",
-        user_prompt = "~",
-        chat_layout = "vsplit",
+
+        -- UI 配置
+        user_prompt = "~ ",
+        -- chat_layout = "float",
         vsplit_win = {
-          width_percentage = 0.35, -- Width of the chat panel (0.2 - 0.7)
+          width_percentage = 0.4,
         },
       }
-      if vim.g.my_deeplx then
-        table.insert(opts.servers, {
-          server = "DeepL",
-          server_type = "translate",
-          models = {
-            {
-              model = "DeepLX",
-              base_url = vim.g.my_deeplx_base_url,
-              api_key_name = "DEEPLX_API_KEY",
-            },
-          },
-        })
-      end
+
       return opts
     end,
   },
