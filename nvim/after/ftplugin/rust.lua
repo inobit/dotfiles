f2c89b592464fed62b88d3a410a4fb2b7a0c8d00
inobit <inoechoc@gmail.com>
2026-05-22 13:38:52 +0800

    feat(nvim): add more command for rust

--- nvim/after/ftplugin/rust.lua
+++ nvim/after/ftplugin/rust.lua
@@ -6,3 +6,9 @@ end
 require("lib.run").register_run_keymap("cargo run", { cwd = get_root_dir })
 -- cargo check
 require("lib.run").register_run_keymap("cargo check", { cwd = get_root_dir, lhs = "<leader>rc" })
+-- cargo test
+require("lib.run").register_run_keymap("cargo test", { cwd = get_root_dir, lhs = "<leader>rt" })
+-- cargo build
+require("lib.run").register_run_keymap("cargo build", { cwd = get_root_dir, lhs = "<leader>rb" })
+-- cargo clippy
+require("lib.run").register_run_keymap("cargo clippy", { cwd = get_root_dir, lhs = "<leader>rl" })
