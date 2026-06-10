8b12e3aec60be88ab51253783fb1cecdb6982f94
inobit <inoechoc@gmail.com>
2026-06-10 09:43:38 +0800

    refactor(nvim): use format helper

--- nvim/after/lsp/ruff.lua
+++ nvim/after/lsp/ruff.lua
@@ -1,17 +1,9 @@
+local helper = require("lib.utils").lsp_format_helper "ruff"
 return {
   on_attach = function(client, buf)
     -- Disable hover in favor of Pyright
     client.server_capabilities.hoverProvider = false
     -- format on save
-    local group = vim.api.nvim_create_augroup("inobit_ruff_formatter", { clear = false }) -- don't clear autocmds
-    vim.api.nvim_clear_autocmds { group = group, buffer = buf }
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      group = group,
-      buffer = buf,
-      desc = "Format python with ruff on save",
-      callback = function()
-        vim.lsp.buf.format { async = false }
-      end,
-    })
+    helper(client, buf)
   end,
 }
